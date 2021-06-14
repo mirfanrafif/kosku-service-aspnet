bd76be1dfbf62db7a411a0adcbac46ace9ec8112
Irfan Rafif <mirfanrafif17@gmail.com>
2021-06-14 14:17:19 +0700

    ngganti id jadi int

--- Controllers/AnakKosController.cs
+++ Controllers/AnakKosController.cs
@@ -6,47 +6,44 @@ using Kosku.Services;
 namespace Kosku.Controllers
 {
 
-    [ApiController]
-    [Route("[controller]")]
-    public class AnakKosController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class AnakKosController : ControllerBase
+  {
+    [HttpGet]
+    public List<AnakKos> getAll()
     {
-
-
-
-        [HttpGet]
-        public List<AnakKos> getAll()
-        {
-            return AnakKosService.getAllAnakKos();
-        }
-
-        [HttpPost]
-        public IActionResult saveAnakKos(AnakKos anakKos)
-        {
-            var result = AnakKosService.addAnakKos(anakKos);
-
-            return CreatedAtAction(nameof(saveAnakKos), new { id = result._id }, anakKos);
-        }
-
-        [HttpGet("{id}")]
-        public AnakKos findAnakKos(string id)
-        {
-            return AnakKosService.findAnakKos(id);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult updateAnakKos(AnakKos anakKos, string id)
-        {
-            AnakKosService.updateAnakKos(anakKos, id);
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult deleteAnakKos(string id)
-        {
-            AnakKosService.deleteAnakKos(id);
-            return NoContent();
-        }
-
-
+      return AnakKosService.getAllAnakKos();
     }
+
+    [HttpPost]
+    public IActionResult saveAnakKos(AnakKos anakKos)
+    {
+      var result = AnakKosService.addAnakKos(anakKos);
+
+      return CreatedAtAction(nameof(saveAnakKos), new { id = result.id }, anakKos);
+    }
+
+    [HttpGet("{id}")]
+    public AnakKos findAnakKos(int id)
+    {
+      return AnakKosService.findAnakKos(id);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult updateAnakKos(AnakKos anakKos, int id)
+    {
+      AnakKosService.updateAnakKos(anakKos, id);
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult deleteAnakKos(int id)
+    {
+      AnakKosService.deleteAnakKos(id);
+      return NoContent();
+    }
+
+
+  }
 }
--- Services/AnakKosService.cs
+++ Services/AnakKosService.cs
@@ -4,55 +4,55 @@ using Kosku.Model;
 
 namespace Kosku.Services
 {
-    public static class AnakKosService
+  public static class AnakKosService
+  {
+    static List<AnakKos> ListAnakKos { get; }
+
+    static int id = 4;
+
+    static AnakKosService()
     {
-        static List<AnakKos> ListAnakKos { get; }
-
-        static int id = 4;
-
-        static AnakKosService()
-        {
-            ListAnakKos = new List<AnakKos> {
-                new AnakKos {_id = "1", nama = "Irfan Rafif", asal = "Malang", nohp = "0238723482947"},
-                new AnakKos {_id = "2", nama = "Irfan Rafif", asal = "Malang", nohp = "0238723482947"},
-                new AnakKos {_id = "3", nama = "Irfan Rafif", asal = "Malang", nohp = "0238723482947"},
-                new AnakKos {_id = "4", nama = "Irfan Rafif", asal = "Malang", nohp = "0238723482947"},
+      ListAnakKos = new List<AnakKos> {
+                new AnakKos {id = 1, nama = "Irfan Rafif", asal = "Malang", nohp = "0238723482947"},
+                new AnakKos {id = 2, nama = "Irfan Rafif", asal = "Malang", nohp = "0238723482947"},
+                new AnakKos {id = 3, nama = "Irfan Rafif", asal = "Malang", nohp = "0238723482947"},
+                new AnakKos {id = 4, nama = "Irfan Rafif", asal = "Malang", nohp = "0238723482947"},
             };
-        }
-
-        public static List<AnakKos> getAllAnakKos() => ListAnakKos;
-
-        public static AnakKos addAnakKos(AnakKos anakKos)
-        {
-            id++;
-            var data = anakKos;
-            data._id = id.ToString();
-
-            ListAnakKos.Add(anakKos);
-
-            return data;
-        }
-
-        public static AnakKos findAnakKos(string id)
-        {
-            return ListAnakKos.Find(a => a._id == id);
-        }
-
-        public static void updateAnakKos(AnakKos anakKos, string id)
-        {
-            var index = ListAnakKos.FindIndex(a => a._id == id);
-
-            if (index == -1) return;
-
-            ListAnakKos[index] = anakKos;
-        }
-
-        public static void deleteAnakKos(string id)
-        {
-            var anakKos = findAnakKos(id);
-
-            if (anakKos == null) return;
-            ListAnakKos.Remove(anakKos);
-        }
     }
+
+    public static List<AnakKos> getAllAnakKos() => ListAnakKos;
+
+    public static AnakKos addAnakKos(AnakKos anakKos)
+    {
+      id++;
+      var data = anakKos;
+      data.id = id;
+
+      ListAnakKos.Add(anakKos);
+
+      return data;
+    }
+
+    public static AnakKos findAnakKos(int id)
+    {
+      return ListAnakKos.Find(a => a.id == id);
+    }
+
+    public static void updateAnakKos(AnakKos anakKos, int id)
+    {
+      var index = ListAnakKos.FindIndex(a => a.id == id);
+
+      if (index == -1) return;
+
+      ListAnakKos[index] = anakKos;
+    }
+
+    public static void deleteAnakKos(int id)
+    {
+      var anakKos = findAnakKos(id);
+
+      if (anakKos == null) return;
+      ListAnakKos.Remove(anakKos);
+    }
+  }
 }
